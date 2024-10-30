d45e2727499a5c9bce9f95fcd0c470d5612fbe1a
Anthony Konschuh <anthony.konschuh@gmail.com>
2024-10-30 14:53:56 -0700

    poc

--- src/app/components/document-input/document-input.component.ts
+++ src/app/components/document-input/document-input.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-document-input',
@@ -10,18 +11,56 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './document-input.component.html',
   styleUrl: './document-input.component.css'
 })
-export class DocumentInputComponent implements OnInit {
-  formGroup = new FormGroup({
-    input: new FormControl()
-  });
+export class DocumentInputComponent {
+  private _files: Array<{ file: File, url: string }> = [];
 
-  urls: string[] = [];
+  @ViewChild('fileSelector', {read: ElementRef}) inputEl!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
-    // this.formGroup.controls.input.valueChanges.subscribe(v => {
-    //   const img = URL.createObjectURL(v);
-    //   this.urls.push(img);
-    // });
+  @HostListener('change', ['$event.target.files']) emitFiles(event: FileList) {
+    this._files.push(...Object.values(event).map(f => ({ file: f, url: URL.createObjectURL(f) })));
+    this.inputEl.nativeElement.value = '';
   }
 
+  public get files(): Array<{ file: File, url: string }> {
+    return this._files;
+  }
+
+  public get size(): number {
+    return this._files.reduce((a, b) => a + b.file.size, 0);
+  }
+
+  async download() {
+    await this.combineImagesToPDF();
+  }
+
+  async combineImagesToPDF(): Promise<void> {
+    const doc = new jsPDF();
+    const urls = this._files.map(f => f.url);
+
+    for (const imageUrl of urls) {
+      const image = await this.loadImage(imageUrl);
+
+      if (image) {
+        doc.addImage(image, 'PNG', 10, 10, 180, 240);
+        if (imageUrl !== urls[urls.length - 1]) {
+          doc.addPage();
+        }
+      }
+    }
+
+    doc.save('test.pdf');
+  }
+
+  loadImage(url: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+      img.src = url;
+    });
+  }
+
+  remove(ind: number) {
+    this._files.splice(ind, 1);
+  }
 }
